ed6aebc19be74caacb81f5bdd997c2f2a712cc0d
Dennis Renz <dennis@r-renz.de>
2025-11-16 23:43:01 +0100

    Add `tests` to solution and use new extension method syntax

--- src/TestUtilities/Options/OptionExtensions.cs
+++ src/TestUtilities/Options/OptionExtensions.cs
@@ -7,39 +7,42 @@ namespace TestUtilities.Options;
 /// </summary>
 public static class OptionExtensions
 {
-    /// <summary>
-    /// Gets the value from the option if it exists, otherwise returns the provided default value.
-    /// If the option is <see langword="null"/>, it is treated as <see cref="None"/>.
-    /// </summary>
-    public static T GetValueOr<T>(this Option<T>? option, T defaultValue)
+    extension<T>(Option<T>? option)
     {
-        option ??= new None();
-        return option.Match(
-            value => value,
-            none => defaultValue);
-    }
+        /// <summary>
+        /// Gets the value from the option if it exists, otherwise returns the provided default value.
+        /// If the option is <see langword="null"/>, it is treated as <see cref="None"/>.
+        /// </summary>
+        public T GetValueOr(T defaultValue)
+        {
+            option ??= new None();
+            return option.Match(
+                value => value,
+                _ => defaultValue);
+        }
 
-    /// <summary>
-    /// Gets the value from the option if it exists, otherwise returns the result of calling the factory function.
-    /// If the option is <see langword="null"/>, it is treated as <see cref="None"/>.
-    /// </summary>
-    public static T GetValueOr<T>(this Option<T>? option, Func<T> defaultFactory)
-    {
-        option ??= new None();
-        return option.Match(
-            value => value,
-            none => defaultFactory());
-    }
+        /// <summary>
+        /// Gets the value from the option if it exists, otherwise returns the result of calling the factory function.
+        /// If the option is <see langword="null"/>, it is treated as <see cref="None"/>.
+        /// </summary>
+        public T GetValueOr(Func<T> defaultFactory)
+        {
+            option ??= new None();
+            return option.Match(
+                value => value,
+                _ => defaultFactory());
+        }
 
-    /// <summary>
-    /// Gets the value from the option if it exists, otherwise returns default(T).
-    /// If the option is <see langword="null"/>, it is treated as <see cref="None"/>.
-    /// </summary>
-    public static T? GetValueOrDefault<T>(this Option<T>? option)
-    {
-        option ??= new None();
-        return option.Match(
-            value => value,
-            none => default(T));
+        /// <summary>
+        /// Gets the value from the option if it exists, otherwise returns default(T).
+        /// If the option is <see langword="null"/>, it is treated as <see cref="None"/>.
+        /// </summary>
+        public T? GetValueOrDefault()
+        {
+            option ??= new None();
+            return option.Match(
+                value => value,
+                _ => default(T));
+        }
     }
 }
